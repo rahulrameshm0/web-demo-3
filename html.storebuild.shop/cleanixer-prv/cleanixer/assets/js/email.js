e739abcba5a98c64fae128f5e22256a9fbb9dae1
rahulrameshm0 <iamrahulm23@gmail.com>
2025-03-04 19:45:55 +0530

    modified the email section

--- html.storebuild.shop/cleanixer-prv/cleanixer/assets/js/email.js
+++ html.storebuild.shop/cleanixer-prv/cleanixer/assets/js/email.js
@@ -1,23 +1,11 @@
-function sendEmail(event) {
-    event.preventDefault(); // Prevent form from refreshing the page
-
-    let params = {
-        to_email: document.getElementById("email").value, // The email entered in the form (Recipient)
+function sendEmail(){
+    let parms = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         subject: document.getElementById("subject").value,
-        date: document.getElementById("date").value
+        date: document.getElementById("date").value,
     };
+        emailjs.send("service_4d81wow", "template_ha5borb", parms).then(alert("Email sent!"))
 
-    console.log("Sending email to:", params.to_email); // Debugging
+};
 
-    emailjs.send("service_4d81wow", "template_ha5borb", params)
-        .then(response => {
-            alert("Email sent successfully to " + params.to_email);
-            console.log("SUCCESS:", response);
-        })
-        .catch(error => {
-            alert("Failed to send email. Check console.");
-            console.error("FAILED:", error);
-        });
-}
